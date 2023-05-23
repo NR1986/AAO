cb919716bc55a92d0e26ac2c7559e28acbedf5bb
Nino Rossi <43315787+NR1986@users.noreply.github.com>
2023-05-23 16:54:41 +0200

    Update usc_link_zu_newsblog.user.js

--- usc_link_zu_newsblog.user.js
+++ usc_link_zu_newsblog.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leitstellenspiel Erweiterung
 // @namespace    http://tampermonkey.net/
-// @version      1.2.5
+// @version      1.2.6
 // @description  Erweiterungen für das Leitstellenspiel
 // @author       NiRoLP
 // @match        https://www.leitstellenspiel.de/*
@@ -9,8 +9,6 @@
 // @grant        GM_info
 // @grant        unsafeWindow
 // ==/UserScript==
-
-
 (function() {
     'use strict';
 
@@ -33,19 +31,69 @@
     }
 
     function addLinkToNavbar() {
-        const newsLink = document.querySelector('#news_li a');
-        if (!newsLink) return;
+        const navbarRight = document.querySelector('.navbar-nav.navbar-right');
+        if (!navbarRight) return;
+
+        const dropdownMenu = document.createElement('ul');
+        dropdownMenu.className = 'dropdown-menu';
+        dropdownMenu.role = 'menu';
+        dropdownMenu.setAttribute('aria-labelledby', 'news');
+
+        const link1 = document.createElement('a');
+        link1.href = 'https://www.facebook.com/Leitstellenspiel.de/';
+        link1.className = 'mixed-mobile-desktop-external-link';
+        link1.id = 'newspage_facebook';
+        link1.target = '_blank';
+        link1.textContent = 'Facebook Newspage';
+
+        const link2 = document.createElement('a');
+        link2.href = 'https://blog.leitstellenspiel.de/blog/';
+        link2.className = 'lightbox-open mixed-mobile-desktop-external-link';
+        link2.textContent = 'Leitstellenspiel Newspage';
+
+        const listItem1 = document.createElement('li');
+        listItem1.role = 'presentation';
+        listItem1.appendChild(link1);
+
+        const listItem2 = document.createElement('li');
+        listItem2.role = 'presentation';
+        listItem2.appendChild(link2);
+
+        dropdownMenu.appendChild(listItem1);
+        dropdownMenu.appendChild(listItem2);
+
+        const dropdown = document.createElement('li');
+        dropdown.className = 'dropdown';
+        dropdown.id = 'news_li';
 
         const link = document.createElement('a');
-        link.href = 'https://aao-update.nirolp.de/news';
-        link.target = '_blank';
-        link.textContent = 'AAO News';
+        link.href = '#';
+        link.id = 'news';
+        link.role = 'button';
+        link.className = 'dropdown-toggle mixed-mobile-desktop-external-link';
+        link.setAttribute('data-toggle', 'dropdown');
 
-        const listItem = document.createElement('li');
-        listItem.role = 'presentation';
-        listItem.appendChild(link);
+        const img = document.createElement('img');
+        img.alt = 'Google_news_ffffff';
+        img.className = 'navbar-icon';
+        img.src = '/images/google_news_ffffff.svg';
+        img.title = 'News';
 
-        newsLink.parentNode.insertBefore(listItem, newsLink.nextSibling);
+        const span = document.createElement('span');
+        span.className = 'visible-xs';
+        span.textContent = 'News';
+
+        const caret = document.createElement('b');
+        caret.className = 'caret';
+
+        link.appendChild(img);
+        link.appendChild(span);
+        link.appendChild(caret);
+
+        dropdown.appendChild(link);
+        dropdown.appendChild(dropdownMenu);
+
+        navbarRight.appendChild(dropdown);
     }
 
     window.addEventListener('load', function() {
